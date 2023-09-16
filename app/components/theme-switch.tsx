1246721c977c869a904f8495ef05f7ac80d8d63c
Farhan Nazir <farhannazir2660@gmail.com>
2023-09-17 04:01:09 +0800

    fix the toogle dark mode not working properly

--- app/components/theme-switch.tsx
+++ app/components/theme-switch.tsx
@@ -1,14 +1,23 @@
 "use client";
 import { useTheme } from "next-themes";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdOutlineLightMode, MdOutlineNightlight } from "react-icons/md";
 
 export default function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme, resolvedTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <div>
-      {theme === "dark" || resolvedTheme === "dark" ? (
+      {theme === "dark" ? (
         <button
           className=" rounded-md  bg-yellow-400 w-10 h-10 flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
           onClick={() => setTheme("light")}
