2f5781b7f20a3beffa7c27f430f26ac7dd308ed0
Farhan Nazir <farhannazir2660@gmail.com>
2023-09-17 03:47:04 +0800

    fix theme switch

--- app/components/theme-switch.tsx
+++ app/components/theme-switch.tsx
@@ -1,16 +1,28 @@
 "use client";
+import { useTheme } from "next-themes";
 import React from "react";
 import { MdOutlineLightMode, MdOutlineNightlight } from "react-icons/md";
-import { useTheme } from "../context/theme-context";
 
 export default function ThemeSwitch() {
-  const { theme, toggleTheme } = useTheme();
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
   return (
-    <button
-      className=" rounded-md  bg-yellow-400 w-10 h-10 flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
-      onClick={toggleTheme}
-    >
-      {theme === "light" ? <MdOutlineLightMode /> : <MdOutlineNightlight />}
-    </button>
+    <div>
+      {theme === "dark" || resolvedTheme === "dark" ? (
+        <button
+          className=" rounded-md  bg-yellow-400 w-10 h-10 flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+          onClick={() => setTheme("light")}
+        >
+          <MdOutlineNightlight />{" "}
+        </button>
+      ) : (
+        <button
+          className=" rounded-md  bg-yellow-400 w-10 h-10 flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+          onClick={() => setTheme("dark")}
+        >
+          <MdOutlineLightMode />{" "}
+        </button>
+      )}
+    </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,9 @@ import Header from "./components/header";
 import ActiveSectionContextProvider from "./context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/footer";
-import ThemeSwitch from "./components/theme-switch";
+
 import ThemeContextProvider from "./context/theme-context";
+import Providers from "./context/theme-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,18 +22,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="!scroll-smooth">
+    <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
       <body
         className={`${inter.className} bg-yellow-200 dark:bg-slate-900 text-black`}
       >
-        <ThemeContextProvider>
+        <Providers>
           <ActiveSectionContextProvider>
             <Header />
             {children}
             <Footer />
             <Toaster position="top-right" />
           </ActiveSectionContextProvider>
-        </ThemeContextProvider>
+        </Providers>
       </body>
     </html>
   );
